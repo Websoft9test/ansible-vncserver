215a3c13aad39eab19e711b195f85e38252d4021
zengxc <zxc916684@163.com>
2020-07-10 19:07:34 +0800

    main

---
+++ roles/vncserver/files/vncpasswd.sh
@@ -0,0 +1,17 @@
+#! /bin/bash
+vnc_password=123456
+su vnc<<EOF
+[ -f /usr/bin/expect ] || yum -y install expect
+/usr/bin/expect 
+set timeout 200
+spawn vncpasswd 
+expect "Password:"
+send "{{vnc_password}}\r"
+expect "Verify:"
+send "{{vnc_password}}\r"
+expect "(y/n)?"
+send "n\r"
+set timeout 200
+expect eof 
+exit
+EOF
